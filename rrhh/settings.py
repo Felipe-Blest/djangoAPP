154bff3968c84af79d7c77c5a2b5b7711e1010de
duoc <git.duoc@duoc.cl>
2023-05-04 17:20:17 -0400

    dir app agregada a template

--- rrhh/settings.py
+++ rrhh/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'rrhh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'appbkn', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
