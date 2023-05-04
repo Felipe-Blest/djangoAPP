8d2ba845b839a4d81d5c4d2cb8e7ee1cf07a4a65
MatiasTorres0 <101838265+MatiasTorres0@users.noreply.github.com>
2023-05-04 17:59:01 -0400

    views y forms agregados

--- appbkn/views.py
+++ appbkn/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import persona
+from .forms import PersonaForm
+# Create your views here.
+
+
+
 
 # Create your views here.
+def PersonaList(request):  
+    Personas = persona.objects.all()  
+    return render(request,"listaPersonas.html",{'personas':Personas})  
+
+def CrearPersona(request):  
+    if request.method == "POST":  
+        form = PersonaForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save() 
+                model = form.instance
+                return redirect('listaPersonas')  
+            except:  
+                pass  
+    else:  
+        form = PersonaForm()  
+    return render(request,'persona-create.html',{'form':form})  
+
+def ActualizarPersona(request, id):  
+    persona = persona.objects.get(id=id)
+    form = PersonaForm(initial={'rut': persona.rut, 'nombre': persona.nombre, 'apellido': persona.apellido})
+    if request.method == "POST":  
+        form = PersonaForm(request.POST, instance=persona)  
+        if form.is_valid():  
+            try:  
+                form.save() 
+                model = form.instance
+                return redirect('/listaPersonas')  
+            except Exception as e: 
+                pass    
+    return render(request,'persona-update.html',{'form':form})  
+
+def EliminarPersona(request, id):
+    persona = persona.objects.get(id=id)
+    try:
+        persona.delete()
+    except:
+        pass
+    return redirect('listaPersonas')
