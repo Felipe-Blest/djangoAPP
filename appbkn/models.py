43ff07e48023e678709a3a986ff2cd3c1faf2989
duoc <git.duoc@duoc.cl>
2023-05-04 18:44:31 -0400

    Actualizacion de archivos

--- appbkn/models.py
+++ appbkn/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Persona(model.Models):
+class Persona(models.Model):
     rut = models.IntegerField(db_column='rut', blank=False)
     nombre = models.TextField(db_column='nombre', blank=False, max_length=20)
     apellido = models.TextField(db_column='apellido', blank=False, max_length=30)
