8ffbf508227b3f8eabf5c982b1819d22d0a26874
MatiasTorres0 <101838265+MatiasTorres0@users.noreply.github.com>
2023-05-11 16:53:28 -0400

    html solucionado

--- appbkn/urls.py
+++ appbkn/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [    
     path('listaPersona', views.PersonaList, name='lista-persona'),
-    path('crearPersona', views.CrearPersona, name='crear-persona'),
+    path('crearPersona/', views.CrearPersona, name='crear-persona'),
     path('persona-modif/<int:id>', views.ActualizarPersona, name='modif-persona'),
     path('persona-borrar/<int:id>', views.EliminarPersona, name='borrar-persona'),
 ]
